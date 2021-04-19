37f21ff3160b964478263cea97b2750c751d1f77
SamMoudry <sammoudry@gmail.com>
2021-04-19 11:05:15 -0500

    render function created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,5 +8,30 @@ function onReady() {
 
 
 function getJokes() {
-    
+    $.ajax({
+        method: 'GET',
+        url: '/jokes'
+    })
+        .then(function(response){
+            console.log('Response from server', response);
+            render(response);
+        })
+        .catch(function(error){
+            console.log('Error', error);
+            alert('Did not get it');
+        })
+        console.log('After making server request...');
+}
+
+function render(array){
+    for (let i = 0; i < array.length; i++) {
+        $('#outputDiv').append(`
+        <div>
+            <p>${array[i].name}</p>
+            <p>${array[i].jokeQuestion}</p>
+            <p>${array[i].punchLine}</p>
+        </div>
+        `)
+        
+    }
 }
